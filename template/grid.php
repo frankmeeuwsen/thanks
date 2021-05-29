aa499cbe527827e0b239dc6f4ee5e7ce4661e6a4
Frank Meeuwsen <frank@incredibleadventure.nl>
2021-05-29 12:34:46 +0200

    logo_lokaal update

--- template/grid.php
+++ template/grid.php
@@ -15,7 +15,7 @@
         <img src="<?php echo get_the_post_thumbnail_url(get_the_id(),'tfs-header') ?: 'https://picsum.photos/300/200?nocache='.microtime();?>" width="300" height="200" class="tfs-imageblock">
         <?php
         echo '</div>';
-        echo '<div class="tfs-related-grid__avatar"><img src="'.get_field('logo').'" width="32" height="32" class="tfs-related-grid__logo"></div>';
+        echo '<div class="tfs-related-grid__avatar"><img src="'.get_field('logo_lokaal').'" width="32" height="32" class="tfs-related-grid__logo"></div>';
         echo '<div class="tfs-related-grid__tile-content"><h4><a href="'.get_permalink().'">'.get_the_title().'</a></h4></div>';
         echo '<div class="tfs-related__content">'.get_the_content().'</div>';
         echo'</a>';
